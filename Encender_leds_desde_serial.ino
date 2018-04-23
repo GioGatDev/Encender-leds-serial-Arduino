7984cf95e080365e05f48ec90c12badd3ed72489
GiovanniGatDev <giovannigatfut@gmail.com>
2018-04-22 20:00:26 -0500

    Proyecto para encender leds mediante el puerto serial

---
+++ Encender_leds_desde_serial.ino
@@ -0,0 +1,70 @@
+int led1= 2;
+int led2= 3;
+int led3= 4;
+char input;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (Serial.available() > 0)
+{
+  input = Serial.read();
+  
+
+  if(input == '1')
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW );
+    digitalWrite(led3, LOW);
+    Serial.println(input);
+    Serial.println("Led 1 Encendido");
+    
+    }
+    if(input == '2')
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH );
+    digitalWrite(led3, LOW);
+    Serial.println(input);
+    Serial.println("Led 2 Encendido");
+    
+    }
+    if(input == '3')
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    Serial.println(input);
+    Serial.println("Led 3 Encendido");
+    
+    }
+    if(input == 'a')
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    Serial.println(input);
+    Serial.println("Leds apagados");
+    
+    }
+if(input == 'e')
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    Serial.println(input);
+    Serial.println("Leds Encendidos");
+    
+    }
+  
+  
+  }
+}
