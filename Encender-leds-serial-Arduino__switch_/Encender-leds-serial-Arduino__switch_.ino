fde6a4a4f7177245abecde3894e98fd9c9e4c854
Giovanni <giovannigatfut@gmail.com>
2018-06-08 14:40:58 -0500

    Agregando la programacion con switch
    Se agrega la programacion con switch y se agrega el apagado de los led desde serial

--- Encender-leds-serial-Arduino__switch_/Encender-leds-serial-Arduino__switch_.ino
+++ Encender-leds-serial-Arduino__switch_/Encender-leds-serial-Arduino__switch_.ino
@@ -4,7 +4,6 @@ int led3= 4;
 char input;
 
 void setup() {
-  // put your setup code here, to run once:
 pinMode(2, OUTPUT);
 pinMode(3, OUTPUT);
 pinMode(4, OUTPUT);
@@ -13,58 +12,66 @@ Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 if (Serial.available() > 0)
 {
   input = Serial.read();
-  
 
-  if(input == '1')
-  {
+
+switch (input){
+
+    case '1':
     digitalWrite(led1, HIGH);
-    digitalWrite(led2, LOW );
-    digitalWrite(led3, LOW);
     Serial.println(input);
     Serial.println("Led 1 Encendido");
-    
-    }
-    if(input == '2')
-  {
-    digitalWrite(led1, LOW);
+    break;
+
+    case '2':
     digitalWrite(led2, HIGH );
-    digitalWrite(led3, LOW);
     Serial.println(input);
     Serial.println("Led 2 Encendido");
-    
-    }
-    if(input == '3')
-  {
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
+    break;
+
+    case '3':
     digitalWrite(led3, HIGH);
     Serial.println(input);
     Serial.println("Led 3 Encendido");
-    
-    }
-    if(input == 'a')
-  {
+    break;
+     case '4':
+    digitalWrite(led1, LOW);
+    Serial.println(input);
+    Serial.println("Led 1 Apagado");
+    break;
+     case '5':
+    digitalWrite(led2, LOW);
+    Serial.println(input);
+    Serial.println("Led 2 Apagado");
+    break;
+     case '6':
+    digitalWrite(led3, LOW);
+    Serial.println(input);
+    Serial.println("Led 3 Apagado");
+    break;
+
+    case 'a':
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
     Serial.println(input);
     Serial.println("Leds apagados");
-    
-    }
-if(input == 'e')
-  {
+    break;
+
+    case 'e':
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
     digitalWrite(led3, HIGH);
     Serial.println(input);
     Serial.println("Leds Encendidos");
+    break;
+
+    default:
+    Serial.println(input);
+    break;
     
     }
-  
-  
-  }
+}
 }
